fba7213aade0528a1aeda8cdaaad78b0ab08adf0
Ulas Tuerkmen <ulas.tuerkmen@gmail.com>
2019-02-12 07:13:26 +0100

    Use enum for size, free the swaps array

--- eight_cython.pyx
+++ eight_cython.pyx
@@ -8,6 +8,9 @@ cdef struct BoardPosition:
     int row
     int column
 
+cdef enum:
+    SIZE = 3
+
 cdef class State:
 
     cdef int **board
@@ -23,31 +26,31 @@ cdef class State:
             if not line:
                 continue
             numbers = [int(x) if x != 'X' else 0 for x in line.split()]
-            for ondex in range(3):
+            for ondex in range(SIZE):
                 retval.board[index][ondex] = numbers[ondex]
         retval.parent = None
         return retval
 
     def __cinit__(self):
         cdef int index
-        self.board = <int **>malloc(3*sizeof(int*))
-        for index in range(3):
-            self.board[index] = <int *>malloc(3*sizeof(int))
+        self.board = <int **>malloc(SIZE*sizeof(int*))
+        for index in range(SIZE):
+            self.board[index] = <int *>malloc(SIZE*sizeof(int))
         self._zero_index.row = -1
 
     def __str__(self):
         parts = []
-        for x in range(3):
+        for x in range(SIZE):
             row = self.board[x]
-            parts.append(' '.join([str(row[y]) for y in range(3)]))
+            parts.append(' '.join([str(row[y]) for y in range(SIZE)]))
         return "\n".join(parts)
 
     cdef BoardPosition zero_index(self):
         cdef int i, j
         if self._zero_index.row != -1:
             return self._zero_index
-        for i in range(3):
-            for j in range(3):
+        for i in range(SIZE):
+            for j in range(SIZE):
                 if self.board[i][j] == 0:
                     self._zero_index = BoardPosition(i, j)
                     return self._zero_index
@@ -56,8 +59,8 @@ cdef class State:
 
     cdef set_board(self, int **parent_board, BoardPosition zero_pos, BoardPosition swap):
         cdef int index
-        for index in range(3):
-            memcpy(self.board[index], parent_board[index], 3*sizeof(int))
+        for index in range(SIZE):
+            memcpy(self.board[index], parent_board[index], SIZE*sizeof(int))
         self.board[zero_pos.row][zero_pos.column] = self.board[swap.row][swap.column]
         self.board[swap.row][swap.column] = 0
 
@@ -67,13 +70,12 @@ cdef class State:
         cdef BoardPosition *swaps = <BoardPosition *>malloc(4*sizeof(BoardPosition))
         cdef int swaps_counter = 0, index = 0
         cdef State child
-        #cdef PyObject **children
         #left
         if zi.column > 0:
             swaps[swaps_counter] = BoardPosition(zi.row, zi.column - 1)
             swaps_counter += 1
         #right
-        if zi.column < 2:
+        if zi.column < (SIZE - 1):
             swaps[swaps_counter] = BoardPosition(zi.row, zi.column + 1)
             swaps_counter += 1
         #above
@@ -81,15 +83,15 @@ cdef class State:
             swaps[swaps_counter] = BoardPosition(zi.row - 1, zi.column)
             swaps_counter += 1
         #below
-        if zi.row < 2:
+        if zi.row < (SIZE - 1):
             swaps[swaps_counter] = BoardPosition(zi.row + 1, zi.column)
             swaps_counter += 1
-        #children = <PyObject **>malloc(swaps_counter*sizeof(PyObject *))
         children = []
         for index in range(swaps_counter):
             child = State()
             child.set_board(self.board, zi, swaps[index])
             children.append(child)
+        free(swaps)
         return children
 
 if __name__ == "__main__":
